155e9652b4b0f7091e8b2db5fe8b5e41a6a573a6
Åke Ek <svein.ek@stud.noroff.no>
2022-12-14 00:30:29 +0100

    changes to index.mjs

--- js/index.mjs
+++ js/index.mjs
@@ -1,19 +1,20 @@
 import { submitUserValues } from "./api/registerUser.mjs";
-
+import { submitLoginUser } from "./api/login.mjs";
+import { findProfile } from "./api/findProfile.mjs";
+import { showListings } from "./api/posts/showListings.mjs";
+import { user } from "./api/findProfile.mjs";
+import { sendPost } from "./api/posts/newPost.mjs";
 
 export const path = location.pathname;
-export const BASE_API_URL = "https://api.noroff.dev/api/v1";
+export const BASE_API_URL = "https://api.noroff.dev";
 
 if (path === "/register.html") {
     submitUserValues();
 } else if (path === "/login.html") {
-    //submitLoginUser();
+    submitLoginUser();
 } else if (path === "/profile.html") {
-    // findProfile(`${BASE_API_URL}/api/v1/social/profiles/${user}?_posts=true`);
-    // sendPost();
-    // submitUpdatedPost();
-} else if (path === "/index.html") {
-    //getPosts(`${BASE_API_URL}/api/v1/social/posts?_author=true&_comments=true&_reactions=true`);
-} else if (path === "/post-by-id.html") {
-    //postById(`${BASE_API_URL}/api/v1/social/posts/${id}?_author=true&_comments=true&_reactions=true`);
-}
+    findProfile(`${BASE_API_URL}/api/v1/auction/profiles/${user}?_posts=true`);
+    showListings(`${BASE_API_URL}/api/v1/auction/profiles/${user}/listings`)
+    sendPost();
+    //   submitUpdatedPost();
+} 
