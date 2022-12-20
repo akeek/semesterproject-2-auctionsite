a0f8828ae8f905e42b8a2ea2f3b1fe3de242f28c
Åke Ek <svein.ek@stud.noroff.no>
2022-12-20 21:12:36 +0100

    added variables

--- js/variables/variables.mjs
+++ js/variables/variables.mjs
@@ -1,6 +1,9 @@
 // API URLs
-export const API_BASE_URL = "https://nf-api.onrender.com";
+export const API_BASE_URL = "https://api.noroff.dev";
 export const API_LISTINGS_URL = "/api/v1/auction/listings";
 export const API_PROFILE_URL = "/api/v1/auction/profiles";
 export const API_REGISTER_URL = "/api/v1/auction/auth/register";
 export const API_LOGIN_URL = "/api/v1/auction/auth/login";
+
+// Forms
+export const createListingForm = document.getElementById("post-form");
