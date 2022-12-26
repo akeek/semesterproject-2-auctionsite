a286188cb4225fdedf3d08a374e0c5d8a33bfbb3
Åke Ek <svein.ek@stud.noroff.no>
2022-12-26 14:05:16 +0100

    rerouting, word changes for easier understanding

--- js/profile/my-listings.mjs
+++ js/profile/my-listings.mjs
@@ -1,6 +1,6 @@
 export function showMyListings(myListings) {
-    const cardsContainer = document.querySelector("#listings-container");
-    cardsContainer.innerHTML = ``;
+    const listingsContainer = document.querySelector("#listings-container");
+    listingsContainer.innerHTML = ``;
 
     for (let i = 0; i < myListings.length; i++) {
         const formattedPostDate = new Date(myListings[i].created).toLocaleDateString("en-us", {
@@ -8,16 +8,15 @@ export function showMyListings(myListings) {
             day: "numeric",
         });
         const formattedPostTime = new Date(myListings[i].created).toLocaleTimeString("en-GB", { hour: "2-digit", minute: "2-digit" });
-
         const formattedDeadlineDate = new Date(myListings[i].endsAt).toLocaleDateString("en-us", { month: "short", day: "numeric" });
         const formattedDeadlineTime = new Date(myListings[i].endsAt).toLocaleTimeString("en-GB", { hour: "2-digit", minute: "2-digit" });
 
-        cardsContainer.innerHTML += `
+        listingsContainer.innerHTML += `
           <div class="col p-2">
                   <div class="card h-100 border-1 bg-light border-0 ">
-                      <a href="/item.html?id=${myListings[i].id}"><img src="${myListings[i].media[0]}" class="card-img-top" style="height: 15rem; object-fit: cover" alt="Image caption: ${myListings[i].title}"></a>
+                      <a href="/listing.html?id=${myListings[i].id}"><img src="${myListings[i].media[0]}" class="card-img-top" style="height: 15rem; object-fit: cover" alt="Image caption: ${myListings[i].title}"></a>
                       <div class="card-body">
-                      <a href="/item.html?id=${myListings[i].id}" class="text-decoration-none"><h3 class="card-title pb-0">${myListings[i].title}</h3></a>
+                      <a href="/listing.html?id=${myListings[i].id}" class="text-decoration-none"><h3 class="card-title pb-0">${myListings[i].title}</h3></a>
                       <p class="card-text">${myListings[i].description}</p>
                       
                       </div>
@@ -26,7 +25,7 @@ export function showMyListings(myListings) {
                       <p class="mb-0"><strong>Bids: </strong>${myListings[i].bids.length}</p>
                       <p class="mb-0"><strong>Created: </strong>${formattedPostDate} <span class="small text-muted">| ${formattedPostTime}</span></p>
                       <p class="mb-3"><strong>Ends at: </strong>${formattedDeadlineDate} <span class="small text-muted">| ${formattedDeadlineTime}</span></p>
-                      <a href="/item.html?id=${myListings[i].id}"><button class="btn btn-primary px-4 mb-2"><strong>Open</strong></button></a>
+                      <a href="/listing.html?id=${myListings[i].id}"><button class="btn btn-primary px-4 mb-2"><strong>Open</strong></button></a>
                       </div>
                   </div>
                   </div>
