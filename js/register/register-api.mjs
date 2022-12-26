3ea7e9e82f961ec5c36e294c854dabb00dcf6d12
Åke Ek <svein.ek@stud.noroff.no>
2022-12-26 14:06:05 +0100

    updated register

---
+++ js/register/register-api.mjs
@@ -0,0 +1,21 @@
+import { registerUserURL } from "../api-stuff/base-urls.mjs";
+import { methodsOfFetch } from "../api-stuff/fetch-method.mjs";
+const { registerUser } = methodsOfFetch;
+
+export async function registerNewUser(registerURL, newUserObject) {
+    try {
+        registerUser.body = JSON.stringify(newUserObject);
+        const response = await fetch(registerUserURL, registerUser);
+        const json = await response.json();
+
+        if (response.status !== 201) {
+            const errorMessage = document.querySelector("#error-message");
+            errorMessage.style.display = "block";
+            errorMessage.innerHTML = `${json.errors[0].message}`;
+        } else {
+            location.href = "/login.html";
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
---
+++ js/register/register.mjs
@@ -0,0 +1,19 @@
+localStorage.clear();
+
+import { registerNewUser } from "./register-api.mjs";
+import { registerUserURL } from "../api-stuff/base-urls.mjs"
+
+const signUpForm = document.querySelector("#signupform");
+const errorMessage = document.querySelector("#error-message");
+errorMessage.style.display = "none";
+
+signUpForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // Create object from form input fields
+    const myFormData = new FormData(event.target);
+    const newUserObject = Object.fromEntries(myFormData.entries());
+
+    // Register new user in API
+    registerNewUser(registerUserURL, newUserObject);
+});
