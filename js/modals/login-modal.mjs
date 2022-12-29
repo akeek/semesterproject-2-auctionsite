18a40ae8a45ea92c64460fc2cb31e4681f225e5c
Åke Ek <svein.ek@stud.noroff.no>
2022-12-30 00:49:16 +0100

    remove useless code

--- js/modals/login-modal.mjs
+++ js/modals/login-modal.mjs
@@ -1,11 +1,5 @@
 var loginModal = document.getElementById("loginModal");
 
-var span = document.getElementsByClassName("close")[0];
-
-// span.onclick = function () {
-//     loginModal.style.display = "none";
-// }
-
 window.onclick = function (event) {
     if (event.target == loginModal) {
         loginModal.style.display = "none";
